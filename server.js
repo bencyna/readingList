4da0389c040cb8f5262f3bf0cdee36890789e428
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2021-05-05 09:12:39 +0930

    changed url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase",
+  process.env.MONGODB_URI || "mongodb://localhost/herokuPractice",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
